029a39830a600bdda82a4f588a2cca66a566e987
Felipe Neves de Arruda <50635026+FelipeDwD@users.noreply.github.com>
2019-12-23 18:13:44 -0300

    2019-12-23

--- RollBack/Program.cs
+++ RollBack/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.IO;
 
-namespace RollBack
+namespace RollBackFiles
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DirectoryInfo directorySentDelete = new DirectoryInfo(@"C:\Users\FelipeN\Desktop\direct\source\sent");
+            DirectoryInfo directoryDestinyRollback = new DirectoryInfo(@"C:\Users\FelipeN\Desktop\direct\destiny");
+            string directorySource = @"C:\Users\FelipeN\Desktop\direct\source\";
+
+
+            foreach (FileInfo file in directorySentDelete.GetFiles())
+            {
+                file.Delete();
+            }
+
+            foreach (FileInfo file in directoryDestinyRollback.GetFiles())
+            {
+                File.Move(file.FullName, directorySource + file.Name);
+            }
+
+            Console.WriteLine("Rollback executado com sucesso!" +
+                "\nTodos os arquivos da pasta sent foram deletados!" +
+                "\nTodos arquivos da pasta destiny retornou para a pasta source");
+
+            Console.ReadKey();
         }
     }
 }
